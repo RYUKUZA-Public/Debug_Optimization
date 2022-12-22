a5655869dcce45472f70d8bad42a96b287c2f690
Ryu <hakrimryu@gmail.com>
2022-12-22 17:56:51 +0900

    Animation Blendingテストデータ削除

--- Assets/00_RF/01_Scripts/Controllers/PlayerController.cs
+++ Assets/00_RF/01_Scripts/Controllers/PlayerController.cs
@@ -9,7 +9,6 @@ public class PlayerController : MonoBehaviour
     //private bool _moveToDest = false;
     // 목적지 위치
     private Vector3 _destPos;
-    private float waitRunRatio = 0f;
     /// <summary>
     /// 플레이어 상태
     /// </summary>
@@ -41,14 +40,31 @@ public class PlayerController : MonoBehaviour
                 break;
         }
     }
+    
+    private void OnMouseClicked(Define.MouseEvent evt)
+    {
+        
+        if (_state == PlayerState.Die)
+            return;
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.magenta, 1.0f);
+        
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Wall")))
+        {
+            // 목적지를 저장후 플레이어가 이동해야 함
+            // 목적지 벡터를 취득
+            _destPos = hit.point;
+            // 마우스 이동 true
+            _state = PlayerState.Moving;
+        }
+    }
 
     private void UpdateIdle()
     {
         // 애니메이션
-        waitRunRatio = Mathf.Lerp(waitRunRatio, 0, 10 * Time.deltaTime);
         Animator anime = GetComponent<Animator>();
-        anime.SetFloat("Wait_run_ratio", waitRunRatio);
-        anime.Play("WAIT_RUN");
     }
     
     private void UpdateMoving()
@@ -77,33 +93,13 @@ public class PlayerController : MonoBehaviour
         }
         
         // 이동 애니메이션
-        waitRunRatio = Mathf.Lerp(waitRunRatio, 1, 10 * Time.deltaTime);
+        
         Animator anime = GetComponent<Animator>();
-        anime.SetFloat("Wait_run_ratio", waitRunRatio);
-        anime.Play("WAIT_RUN");
     }
 
     private void UpdateDie()
     {
     }
     
-    private void OnMouseClicked(Define.MouseEvent evt)
-    {
-        
-        if (_state == PlayerState.Die)
-            return;
-        
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100f, Color.magenta, 1.0f);
-        
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Wall")))
-        {
-            // 목적지를 저장후 플레이어가 이동해야 함
-            // 목적지 벡터를 취득
-            _destPos = hit.point;
-            // 마우스 이동 true
-            _state = PlayerState.Moving;
-        }
-    }
+    
 }
