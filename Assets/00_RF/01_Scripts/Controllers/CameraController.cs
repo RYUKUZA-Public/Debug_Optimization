66d0d8cb9bc2fbfef8ac49780d8db16c867da111
Ryu <hakrimryu@gmail.com>
2022-12-22 11:34:02 +0900

    [CameraController] プレイヤーが、壁に隠された時、プレイヤーを表示する機能作成

--- Assets/00_RF/01_Scripts/Controllers/CameraController.cs
+++ Assets/00_RF/01_Scripts/Controllers/CameraController.cs
@@ -21,10 +21,22 @@ public class CameraController : MonoBehaviour
     {
         if (_mode == Define.CameraMode.QuarterView)
         {
-            // 카메라 포지션 (플레이어 기준 + 델타값)
-            transform.position = _player.transform.position + this._delta;
-            // 로테이션은 LookAt을 사용하여 플레이어를 바라보게 한다.
-            transform.LookAt(_player.transform);
+            // 벽을 확인
+            RaycastHit hit;
+            if (Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))
+            {
+                // 여기서 카메라를 이동시킴
+                // 충돌한 좌표 - 플레이의 포지션 = 방향벡터 .magnitude = 방향텍터의 크기
+                float dist = (hit.point - _player.transform.position).magnitude * .8f;
+                transform.position = _player.transform.position + _delta.normalized * dist;
+            }
+            else
+            {
+                // 카메라 포지션 (플레이어 기준 + 델타값)
+                transform.position = _player.transform.position + this._delta;
+                // 로테이션은 LookAt을 사용하여 플레이어를 바라보게 한다.
+                transform.LookAt(_player.transform);
+            }
         }
     }
     
