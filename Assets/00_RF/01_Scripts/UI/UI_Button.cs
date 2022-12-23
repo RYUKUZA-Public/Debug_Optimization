3a46e42bc615540d28ce2f3182f6fbaefc1a794a
Ryu <hakrimryu@gmail.com>
2022-12-23 11:36:31 +0900

    [UI_Button] GameObjectのBind機能作成

--- Assets/00_RF/01_Scripts/UI/UI_Button.cs
+++ Assets/00_RF/01_Scripts/UI/UI_Button.cs
@@ -10,7 +10,7 @@ public class UI_Button : MonoBehaviour
     /// 어떤 타입인지 모르기 때문에 Text, Button 등의 최상위 부모를 사용
     /// 타입을 넣어 주면 해당 타입의 목록을 취득
     /// </summary>
-    private Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, Object[]>();
+    private Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, Object[]>(); 
 
     /// <summary>
     /// enum의 이름은 오브젝트의 이름과 동일 해야 한다.
@@ -24,11 +24,16 @@ public class UI_Button : MonoBehaviour
         PointText,
         ScoreText
     }
+    enum GameObjects
+    {
+        TestObject
+    }
     
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
 
         // Text타입의 ScoreText의 text를 변경
         Get<Text>((int)Texts.ScoreText).text = "Bind Test";
@@ -43,13 +48,22 @@ public class UI_Button : MonoBehaviour
         string[] names = Enum.GetNames(type);
         // 취득한 수 만큼 배열 생성
         UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        // 딕셔너리에 추가
+        _objects.Add(typeof(T), objects);
         
         // 해당 자식들을 찾아서 취득
         for (int i = 0; i < names.Length; i++)
-            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-        
-        // 딕셔너리에 추가
-        _objects.Add(typeof(T), objects);
+        {
+            // GameObject
+            if (typeof(T) == typeof(GameObject))
+                objects[i] = Util.FindChild(gameObject, names[i], true);
+            // component
+            else
+                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+
+            if (objects[i] == null)
+                Debug.Log($"로드 실패 : {names[i]}");
+        }
     }
 
     /// <summary>
