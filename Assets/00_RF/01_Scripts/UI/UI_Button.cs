c0739e5520fd902e930057a1f38abdcce593dc68
Ryu <hakrimryu@gmail.com>
2022-12-23 14:32:06 +0900

    [UI_Button] BindでイメージをGetし、Event Handlerを利用した、ドラッグ機能作成

--- Assets/00_RF/01_Scripts/UI/UI_Button.cs
+++ Assets/00_RF/01_Scripts/UI/UI_Button.cs
@@ -20,15 +20,29 @@ public class UI_Button : UI_Base
     {
         TestObject
     }
+
+    enum Images
+    {
+        TestImage
+    }
     
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
         Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
 
         // Text타입의 ScoreText의 text를 변경
         GetText((int)Texts.ScoreText).text = "Bind Test";
+        
+        // Test Image의 Handler를 받아 와서 드래그 추가
+        GameObject go = GetImage((int)Images.TestImage).gameObject;
+        UI_EventHandler evt = go.GetComponent<UI_EventHandler>();
+        evt.OnDragHandler += data =>
+        {
+            go.transform.position = data.position;
+        };
     }
     
     private int _score = 0;
