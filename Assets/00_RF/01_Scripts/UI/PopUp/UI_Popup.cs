c8194e8b3a68c61195bf6a7c278c4e256310c282
RYU <hakrimryu@gmail.com>
2022-12-25 21:49:39 +0900

    [整理] UI_Manager 作成前の整理

---
+++ Assets/00_RF/01_Scripts/UI/PopUp/UI_Popup.cs
@@ -0,0 +1,13 @@
+
+public class UI_Popup : UI_Base
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/00_RF/01_Scripts/UI/Scene/UI_Scene.cs
@@ -0,0 +1,16 @@
+
+
+public class UI_Scene : UI_Base
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
